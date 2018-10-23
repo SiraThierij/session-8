810897cfd3473c3bec5022699013e5bb14e97ad6
Sira Thierij <sirathierij@Siras-MBP.sciences-po.fr>
2018-10-23 12:12:44 +0200

    New resume
    New resume

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 var app = new Vue({
   el: '#app',
   data: {
-    firstName: "Jules",
-    lastName: "Bonnard",
-    description: "Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
+    firstName: "Sira",
+    lastName: "Thiery",
+    description: "Master student in Journalism and International Security at Sciences Po Paris.",
     photo: "https://pbs.twimg.com/profile_images/899285838516936704/nfeLD7he_400x400.jpg",
-    twitter: "julesbonnard",
-    github: "julesbonnard",
-    mail: "julesbonnard@gmail.com",
+    twitter: "sirathierij",
+    github: "sirathierij",
+    mail: "sira.thierij@sciencespo.fr",
     experiences: [{
-      dateBegin: "2014",
-      dateEnd: "Today",
-      name: "Agence France Presse",
-      title: "Datajournalist",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      dateBegin: "07/2018",
+      dateEnd: "09/2018",
+      name: "Deutsche Welle TV, Studio Washington, D.C.",
+      title: "Videojournalist",
+      description: "Creating TV reports in New Mexico, Kansas, Florida and Washington, D.C. as well as writing articles, producing webvideos and conducting research."
     }, {
-      dateBegin: "Été 2013",
-      dateEnd: false,
-      name: "Le Monde.fr",
-      title: "Data-journalist",
-      description: false
+      dateBegin: "05/2018",
+      dateEnd: "06/2018",
+      name: "Syria Direct, Amman, Jordan",
+      title: "Videojournalist",
+      description: "Creating a short documentary about Syrian refugees in Jordan as well as a video about Syria Direct which is used for fundraising activities."
     }, {
-      dateBegin: "2012",
-      dateEnd: "2013",
-      name: "Le Dauphiné Libéré",
-      title: "Stagiaire et correspondant de presse",
-      description: false
+      dateBegin: "2016",
+      dateEnd: "2017",
+      name: "Schwarzkopf Foundation",
+      title: "Seminar teacher",
+      description: "Teaching high school students all over Germany about politics and the European Union, thereby boosting political participation."
     }],
     formations: [{
-      dateBegin: "2012",
-      dateEnd: "2014",
-      name: "Master Journalisme",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2017",
+      dateEnd: "2019",
+      name: "Joint Master in Journalism and International Security",
+      university: "Sciences Po Paris"
     }, {
-      dateBegin: "2009",
-      dateEnd: "2012",
-      name: "Bachelor Sciences Politiques",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2013",
+      dateEnd: "2017",
+      name: "BSc in European Studies and Law",
+      university: "Technical University of Twente"
     }],
-    languages: ["Anglais"],
-    skills: ["Node", "VueJS", "D3js", "SCSS", "HTML"]
+    languages: ["English", "German", "French", "Dutch", "Arabic"],
+    skills: ["Adobe Premiere Pro", "Adobe Audition"],
   },
   computed: {
     fullName() {
